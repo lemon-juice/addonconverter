64efdbc144a40eb99b0c98c9164618fc385bc8be
Michal <michal.lemon@gmail.com>
2014-05-17 15:12:30 +0200

    error page

---
+++ error404.php
@@ -0,0 +1,12 @@
+<? include "templates/header.php" ?>
+
+<h2>Page Not Found</h2>
+
+<? if (strpos($_SERVER['REQUEST_URI'], '/tmp/convert/') !== false): ?>
+	<p>The file you were trying to download is no longer available. Probably your session has expired. Converted add-ons are deleted after 15 minutes. Please <a href=".">go to home page</a> and do the conversion again.</p>
+
+<? else: ?>
+	<p style="margin-top: 2em"><a href=".">« Go to home page</a></p>
+<? endif ?>
+
+<? include "templates/footer.php" ?>
