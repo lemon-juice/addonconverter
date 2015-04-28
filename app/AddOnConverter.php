fb8985f78bc79612021838f3109c22f6c534b142
Michal <michal.lemon@gmail.com>
2015-04-28 13:11:27 +0200

    'chrome://browser/skin/livemark-folder.png' => 'chrome://communicator/skin/bookmarks/livemark-folder.png'

--- app/AddOnConverter.php
+++ app/AddOnConverter.php
@@ -85,6 +85,7 @@ class AddOnConverter {
 			'chrome://browser/locale/places/places.dtd' => 'chrome://communicator/locale/bookmarks/places.dtd',
 			'chrome://browser/content/places/bookmarkProperties.xul' => 'chrome://communicator/content/bookmarks/bm-props.xul',
 			'chrome://browser/content/places/bookmarkProperties2.xul' => 'chrome://communicator/content/bookmarks/bm-props.xul',
+			'chrome://browser/skin/livemark-folder.png' => 'chrome://communicator/skin/bookmarks/livemark-folder.png',
 			'chrome://browser/content/' => 'chrome://navigator/content/',
 			'resource:///modules/sessionstore/SessionStore.jsm' => 'resource:///components/nsSessionStore.js',
 			//'chrome://browser/locale/preferences/cookies.dtd' => 'chrome://communicator/locale/permissions/cookieViewer.dtd',
